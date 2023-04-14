3e777115cdde1e6e08b6fe1bc314f841e6d5b68d
Fadi Hassanieh <fadihassanieh@live.com>
2023-04-14 21:35:50 +0300

    Added Migrations Folder

---
+++ CMPS278Backend/Migrations/CMPS278Data/20230409183830_CreateData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMPS278Backend.Migrations.CMPS278Data
+{
+    /// <inheritdoc />
+    public partial class CreateData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BooksDatas",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(10)", unicode: false, maxLength: 10, nullable: false),
+                    Title = table.Column<string>(type: "varchar(192)", unicode: false, maxLength: 192, nullable: false),
+                    description = table.Column<string>(type: "varchar(3970)", unicode: false, maxLength: 3970, nullable: true),
+                    authors = table.Column<string>(type: "varchar(270)", unicode: false, maxLength: 270, nullable: true),
+                    image = table.Column<string>(type: "varchar(111)", unicode: false, maxLength: 111, nullable: true),
+                    previewLink = table.Column<string>(type: "varchar(411)", unicode: false, maxLength: 411, nullable: true),
+                    publisher = table.Column<string>(type: "varchar(49)", unicode: false, maxLength: 49, nullable: true),
+                    publishedDate = table.Column<string>(type: "varchar(10)", unicode: false, maxLength: 10, nullable: true),
+                    infoLink = table.Column<string>(type: "varchar(261)", unicode: false, maxLength: 261, nullable: true),
+                    categories = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    ratingsCount = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__BooksDat__3214EC07DBA92E56", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BooksReviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Book_ID = table.Column<string>(type: "varchar(10)", unicode: false, maxLength: 10, nullable: false),
+                    Title = table.Column<string>(type: "varchar(192)", unicode: false, maxLength: 192, nullable: true),
+                    Price = table.Column<decimal>(type: "numeric(6,2)", nullable: true),
+                    User_id = table.Column<string>(type: "varchar(21)", unicode: false, maxLength: 21, nullable: true),
+                    profileName = table.Column<string>(type: "varchar(49)", unicode: false, maxLength: 49, nullable: true),
+                    reviewhelpfulness = table.Column<string>(type: "varchar(7)", unicode: false, maxLength: 7, nullable: false),
+                    reviewscore = table.Column<decimal>(type: "numeric(3,1)", nullable: false),
+                    reviewtime = table.Column<int>(type: "int", nullable: false),
+                    reviewsummary = table.Column<string>(type: "varchar(128)", unicode: false, maxLength: 128, nullable: false),
+                    reviewtext = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__BooksRev__3214EC07C302111E", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BooksDatas");
+
+            migrationBuilder.DropTable(
+                name: "BooksReviews");
+        }
+    }
+}
---
+++ CMPS278Backend/Migrations/CMPS278Data/20230411171258_AddGameData.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMPS278Backend.Migrations.CMPS278Data
+{
+    /// <inheritdoc />
+    public partial class AddGameData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameData",
+                columns: table => new
+                {
+                    AppId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Developer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeveloperId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Score = table.Column<double>(type: "float", nullable: true),
+                    ScoreText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PriceText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Free = table.Column<bool>(type: "bit", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescriptionHTML = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Installs = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MinInstalls = table.Column<long>(type: "bigint", nullable: true),
+                    MaxInstalls = table.Column<long>(type: "bigint", nullable: true),
+                    Ratings = table.Column<long>(type: "bigint", nullable: true),
+                    ReviewsCount = table.Column<long>(type: "bigint", nullable: true),
+                    Histogram = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: true),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Available = table.Column<bool>(type: "bit", nullable: true),
+                    OffersIAP = table.Column<bool>(type: "bit", nullable: true),
+                    IAPRange = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AndroidVersion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AndroidVersionText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeveloperInternalID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeveloperEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeveloperWebsite = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeveloperAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GenreId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FamilyGenre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FamilyGenreId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HeaderImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Screenshots = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Video = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VideoImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContentRating = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContentRatingDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdSupported = table.Column<bool>(type: "bit", nullable: true),
+                    Released = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Updated = table.Column<long>(type: "bigint", nullable: true),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecentChanges = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameData", x => x.AppId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameReviews",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GameAppId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Score = table.Column<double>(type: "float", nullable: true),
+                    ScoreText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReplyDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReplyText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ThumbsUp = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameReviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameReviews_GameData_GameAppId",
+                        column: x => x.GameAppId,
+                        principalTable: "GameData",
+                        principalColumn: "AppId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameReviews_GameAppId",
+                table: "GameReviews",
+                column: "GameAppId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameReviews");
+
+            migrationBuilder.DropTable(
+                name: "GameData");
+        }
+    }
+}
