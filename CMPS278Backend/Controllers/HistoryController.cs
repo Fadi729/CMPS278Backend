80b7fb60dff2e5de20345da299c30c3179a38808
BassemAD <79159425+BassemAD@users.noreply.github.com>
2023-05-04 19:28:08 +0300

    work in progress

---
+++ CMPS278Backend/Controllers/HistoryController.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Claims;
+using CMPS278Backend.Extensions;
+using CMPS278Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMPS278Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class HistoryController : ControllerBase
+{
+    readonly CMPS278DataContext _context;
+
+    public HistoryController(CMPS278DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<History>> GetHistory()
+    {
+        History? history = await _context.History.FindAsync(HttpContext.GetUserId());
+
+        if (history == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(history);
+    }
+
+    // POST: api/History
+    [HttpPost]
+    public async Task<ActionResult> AddItemToHistory(HistoryItem item)
+    {
+        string userId = HttpContext.GetUserId();
+        History? history = await _context.History.FindAsync(userId);
+
+        if (history == null)
+        {
+            history = new History
+            {
+                UserId = userId,
+                Items = new List<HistoryItem>()
+            };
+            _context.History.Add(history);
+        }
+
+        history.Items.Add(item);
+        _context.Update(history);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    // DELETE: api/History
+    [HttpDelete]
+    public async Task<ActionResult> RemoveItemFromHistory(string itemId)
+    {
+        History? history = await _context.History.FindAsync(HttpContext.GetUserId());
+
+        if (history == null)
+        {
+            return NotFound();
+        }
+
+        HistoryItem item = history.Items.First(i => i.ItemId == itemId);
+        history.Items.Remove(item);
+        _context.Update(history);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+}
---
+++ CMPS278Backend/Models/History.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMPS278Backend.Models
+{
+	public class History
+	{
+            [Key]
+            public string UserId { get; set; }
+            public ICollection<HistoryItem> Items { get; set; }
+    }
+
+    public class HistoryItem
+    {
+        public string ItemType { get; set; }
+        public string ItemId { get; set; }
+    }
+
+}
+
